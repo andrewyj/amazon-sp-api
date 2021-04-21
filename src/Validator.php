8236e7d8cdd9053795e8786b274174e42e641e01
andrew <andrewyingju@gmail.com>
2021-04-21 11:43:07 +0800

    修复2维以上数组验证正确后返回的数组结构不正确问题

--- src/Validator.php
+++ src/Validator.php
@@ -18,31 +18,23 @@ class Validator
         $this->validated = [];
         $this->validatedArray = [];
         foreach ($rules as $key => $rule) {
-            $ruleItems = explode('|', $rule);
-            if (empty($ruleItems)) {
-                continue;
-            }
             if (strpos($key, '.') > 0) {
                 $keys = explode('.', $key);
-                $value = $values[$keys[0]] ?? null;
-                unset($this->validated[$keys[0]]);
-                $res = $this->arrayValidate($value, $ruleItems, $keys, 0, count($keys) -1);
-                if ($res !== false) {
-                    $this->validatedArray[$keys[0]] = array_merge($this->validatedArray[$keys[0]] ?? [], $res);
+                $index = $keys[0];
+                $value = $values[$index] ?? null;
+                unset($this->validated[$index]);
+                if (!isset($this->validatedArray[$index])) {
+                    $this->validatedArray[$index] = [];
                 }
+                $this->arrayValidate($value, $rule, $keys, 0, count($keys) -1, $this->validatedArray[$index]);
             } else {
-                $shouldPassed = true;
-                foreach ($ruleItems as $ruleItem) {
-                    if (strpos($ruleItem, 'required') === false && !isset($values[$key])) {
-                        continue;
-                    }
-                    $res = $this->singleValidate($values[$key] ?? null, $ruleItem, $key);
-                    if ($res === false) {
-                        $shouldPassed = false;
-                        break;
-                    }
+                if (strpos($rule, 'required') === false && !isset($values[$key])) {
+                    continue;
                 }
-                if ($shouldPassed === true && isset($values[$key])) {
+                if ($this->singleValidate($values[$key] ?? null, $rule, $key) === false) {
+                    break;
+                }
+                if (isset($values[$key])) {
                     $this->validated[$key] = $values[$key];
                 }
             }
@@ -66,97 +58,95 @@ class Validator
         return array_values($this->errors)[0] ?? '';
     }
 
-    protected function singleValidate($value, string $rule, $key): bool
+    protected function singleValidate($value, string $rules, $key): bool
     {
-        $ruleValue = explode(':', $rule);
-        if ($ruleValue[0] === 'required') {
-            if (is_null($value)) {
-                $this->errors[$key] = "{$key} is required";
+        foreach (explode('|', $rules) as $rule) {
+            $ruleValue = explode(':', $rule);
+            if ($ruleValue[0] === 'required') {
+                if (is_null($value)) {
+                    $this->errors[$key] = "{$key} is required";
+                    return false;
+                }
+            } elseif ($ruleValue[0] === 'array') {
+                if (!is_array($value)) {
+                    $this->errors[$key] = "{$key} is not an array";
+                    return false;
+                }
+            } elseif ($ruleValue[0] === 'string') {
+                if (!is_string($value)) {
+                    $this->errors[$key] = "{$key} is not string";
+                    return false;
+                }
+            } elseif ($ruleValue[0] === 'integer') {
+                if (!is_integer($value)) {
+                    $this->errors[$key] = "{$key} is not integer";
+                    return false;
+                }
+            } elseif ($ruleValue[0] === 'min') {
+                $min = $ruleValue[1] ?? 0;
+                $len = is_string($value) ? strlen($value) : (is_array($value) ? count($value) : $value);
+                if ($len < $min) {
+                    $this->errors[$key] = "length of {$key} is less than {$min}";
+                    return false;
+                }
+            } elseif ($ruleValue[0] === 'max') {
+                $max = $ruleValue[1] ?? 0;
+                $len = is_string($value) ? strlen($value) : (is_array($value) ? count($value) : $value);
+                if ($len > $max) {
+                    $this->errors[$key] = "length of {$key} is greater than {$max}";
+                    return false;
+                }
+            } elseif ($ruleValue[0] === 'in') {
+                $enum = explode(',', $ruleValue[1] ?? '');
+                if (!in_array($value, $enum, true)) {
+                    $this->errors[$key] = "value '{$value}' of {$key} not valid";
+                    return false;
+                }
+            } else {
+                $this->errors['rule'] = "unknown rule: '{$ruleValue[0]}'";
                 return false;
             }
-        } elseif ($ruleValue[0] === 'array') {
-            if (!is_array($value)) {
-                $this->errors[$key] = "{$key} is not an array";
-                return false;
-            }
-        } elseif ($ruleValue[0] === 'string') {
-            if (!is_string($value)) {
-                $this->errors[$key] = "{$key} is not string";
-                return false;
-            }
-        } elseif ($ruleValue[0] === 'integer') {
-            if (!is_integer($value)) {
-                $this->errors[$key] = "{$key} is not integer";
-                return false;
-            }
-        } elseif ($ruleValue[0] === 'min') {
-            $min = $ruleValue[1] ?? 0;
-            $len = is_string($value) ? strlen($value) : (is_array($value) ? count($value) : $value);
-            if ($len < $min) {
-                $this->errors[$key] = "{$key} is less than {$min}";
-                return false;
-            }
-        } elseif ($ruleValue[0] === 'max') {
-            $max = $ruleValue[1] ?? 0;
-            $len = is_string($value) ? strlen($value) : (is_array($value) ? count($value) : $value);
-            if ($len > $max) {
-                $this->errors[$key] = "{$key} is greater than {$max}";
-                return false;
-            }
-        } elseif ($ruleValue[0] === 'in') {
-            $enum = explode(',', $ruleValue[1] ?? '');
-            if (!in_array($value, $enum, true)) {
-                $this->errors[$key] = "value {$value} of {$key} not valid";
-                return false;
-            }
-        } else {
-            $this->errors['rule'] = "unknown rule: {$ruleValue[0]}";
-            return false;
         }
 
         return true;
     }
 
-    protected function arrayValidate($value, array $rules, array $keys, int $currentLevel,int $totalLevel)
+    protected function arrayValidate($value, string $rules, array $keys, int $currentLevel,int $totalLevel, &$validated)
     {
         if (empty($value)) {
-            if (in_array('required', $rules, true)) {
+            if (strpos($rules, 'required') !== false) {
                 $key = implode('.', array_slice($keys, 0, ($currentLevel+1)));
-                $this->errors[$key] = "{$key} can not be empty";
+                $this->errors[$key] = "{$key} is required";
             }
+
             return false;
         }
         if ($totalLevel == $currentLevel) {
-            foreach ($rules as $rule) {
-                if ($this->singleValidate($value, $rule, implode('.', $keys)) === false) {
-                    return false;
-                }
-            }
-
-            return $value;
-        }
-        $validated = [];
-        ++$currentLevel;
-        $index = $keys[$currentLevel];
-        if ($index === '*') {
-            $value = (array)$value;
-            foreach ($value as $index => $item) {
-                $res = $this->arrayValidate($item, $rules, $keys, $currentLevel, $totalLevel);
-                if ($res === false) {
-                    return false;
-                }
-                $validated[$index] = $res;
-            }
-        } else {
-            $res = $this->arrayValidate($value[$index] ?? null, $rules, $keys, $currentLevel, $totalLevel);
-            if ($res === false) {
+            if ($this->singleValidate($value, $rules, implode('.', $keys)) === false) {
                 return false;
             }
-            if (isset($value[$index])) {
-                $validated[$index] = $value[$index];
+            $validated = $value;
+
+            return true;
+        }
+        ++$currentLevel;
+        $index = $keys[$currentLevel];
+
+        if ($index !== '*') {
+            $val = $value[$index];
+            $value = [$index => $val ?? null];
+        }
+        foreach ($value as $index => $item) {
+            if (!isset($validated[$index])) {
+                $validated[$index] = [];
+            }
+            $res = $this->arrayValidate($item, $rules, $keys, $currentLevel, $totalLevel, $validated[$index]);
+            if ($res === false) {
+                unset($validated[$index]);
+                return false;
             }
         }
 
-        return $validated;
+        return true;
     }
 }
